8e29ed89f76393dc860f048ea44372d81177566c
Wcsss <caishenwong@gmail.com>
2023-04-17 16:49:46 +0800

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import Navbar from '@/components/Navbar';
+import { UserContext } from '@/lib/context';
+import { useUserData } from '@/lib/hooks';
+import { Toaster } from 'react-hot-toast';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster/>
+    </UserContext.Provider>
+  );
 }
+
+export default MyApp;
